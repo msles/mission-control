da63265e1c7a120c7ecd540613916787b8ba9096
packard-j <packard.ja@husky.neu.edu>
2022-10-03 16:41:10 -0400

    add error message to parse result

--- src/web/parse.ts
+++ src/web/parse.ts
@@ -2,6 +2,8 @@
  * Parses a query to a set of parameters.
  * If the input cannot be parsed (e.g. if it is not valid), return undefined.
  */
-export type Parser<Params> = (query: unknown) => Maybe<Params>;
+export type Parser<Params> = (query: unknown) => ParseResult<Params>;
 
-type Maybe<T> = T | undefined;
+type ParseResult<T> = ParseSuccess<T> | ParseFail;
+type ParseSuccess<T> = {success: true, data: T};
+type ParseFail = {success: false, error: string};
