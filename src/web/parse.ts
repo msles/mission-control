4d5702e5f035e2673dada544715c3ce659330ebb
packard-j <packard.ja@husky.neu.edu>
2022-10-07 10:29:33 -0400

    convenience fn for zod parser

--- src/web/parse.ts
+++ src/web/parse.ts
@@ -1,9 +1,27 @@
+import { Schema } from "zod";
+
 /**
  * Parses a query to a set of parameters.
  * If the input cannot be parsed (e.g. if it is not valid), return undefined.
  */
 export type Parser<Params> = (query: unknown) => ParseResult<Params>;
 
-type ParseResult<T> = ParseSuccess<T> | ParseFail;
+export type ParseResult<T> = ParseSuccess<T> | ParseFail;
 type ParseSuccess<T> = {success: true, data: T};
 type ParseFail = {success: false, error: string};
+
+/**
+ * Convenience function for creating a Parser from a zod Schema.
+ */
+export function createZodParser<T>(schema: Schema<T>): Parser<T> {
+  return (query: unknown) => {
+    const parsed = schema.safeParse(query);
+    if (parsed.success) {
+      return parsed;
+    }
+    else {
+      // TODO: provide a more informative error message
+      return {success: false, error: 'parsing error'};
+    }
+  }
+}
