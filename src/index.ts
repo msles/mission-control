ba3fa93da86a9b70a3351ca1ec63b202fbec5ed0
packard-j <packard.ja@husky.neu.edu>
2022-11-27 20:14:57 -0500

    make draw mode default

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import DrawMode from "./modes/draw";
 import PongMode from "./modes/pong";
 
 new MissionControl([
-  ["pong", PongMode],
-  ["draw", DrawMode]
+  ["draw", DrawMode],
+  ["pong", PongMode]
 ]).start()
   .then(() => console.log('mission control started.'))
   .catch(() => console.warn('failed to start mission control.'));
